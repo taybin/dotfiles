da02dd4442a4a99d53a28203a0916bda69e99077
Taybin Rutkin <trutkin@novetta.com>
2019-11-12 09:49:46 -0500

    add iterm2 integration line for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,6 @@ set PATH ~/bin $PATH
 set fish_greeting
 fish_vi_key_bindings
 
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
 starship init fish | source
