c1b3b216f7ce95e41e468be987785e3837dd40ed
Taybin Rutkin <trutkin@novetta.com>
2019-12-17 15:28:24 -0500

    use nvim for VISUAL, improve pgcli paging

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,9 @@ set PATH ~/bin $PATH
 set fish_greeting
 fish_vi_key_bindings
 
+set EDITOR nvim
+set VISUAL nvim
+
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
 starship init fish | source
