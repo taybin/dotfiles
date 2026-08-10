c3dd3bb12fca9ec5cc219e9dcd471e8b06881719
Taybin Rutkin <taybin.rutkin@skywardfederal.com>
2026-08-09 21:10:52 -0400

    add nvim-tree plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -92,6 +92,10 @@ do
 	-- Enable faster startup by caching compiled Lua modules
 	vim.loader.enable()
 
+	-- turn netrw off to avoid conflicts with nvim-tree
+	vim.g.loaded_netrw = 1
+	vim.g.loaded_netrwPlugin = 1
+
 	-- Set <space> as the leader key
 	-- See `:help mapleader`
 	--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
---
+++ .config/nvim/lua/custom/plugins/treeview.lua
@@ -0,0 +1,6 @@
+vim.pack.add({
+	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
+	{ src = "https://github.com/nvim-tree/nvim-tree.lua" },
+})
+
+require("nvim-tree").setup()
