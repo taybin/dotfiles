e8df27d5dbcec1f44d2520e046cd52d295dcdd95
Taybin Rutkin <taybin.rutkin@skywardfederal.com>
2026-09-09 11:00:18 -0400

    close telescope neovim window on first ESC

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -489,12 +489,13 @@ do
 	require("telescope").setup({
 		-- You can put your default mappings / updates / etc. in here
 		--  All the info you're looking for is in `:help telescope.setup()`
-		--
-		-- defaults = {
-		--   mappings = {
-		--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-		--   },
-		-- },
+		defaults = {
+			mappings = {
+				-- close immediately on <Esc> instead of dropping into
+				-- Telescope's normal mode first (which needs a 2nd <Esc>)
+				i = { ["<esc>"] = require("telescope.actions").close },
+			},
+		},
 		-- pickers = {}
 		extensions = {
 			["ui-select"] = { require("telescope.themes").get_dropdown() },
