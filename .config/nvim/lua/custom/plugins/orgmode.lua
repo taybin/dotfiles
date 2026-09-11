afdbb6c61b34674e834bb1b83108bb3b79179db5
Taybin Rutkin <taybin.rutkin@skywardfederal.com>
2026-09-11 16:37:18 -0400

    add mactex, emacs, and pdf export support for org-mode

--- .config/nvim/lua/custom/plugins/orgmode.lua
+++ .config/nvim/lua/custom/plugins/orgmode.lua
@@ -39,6 +39,25 @@ require("orgmode").setup({
 			target = org_path("meetings/%^{PROMPT}.org"),
 		},
 	},
+	org_custom_exports = {
+		t = {
+			label = "Export to PDF (via typst) format",
+			action = function(exporter)
+				local current_file = vim.api.nvim_buf_get_name(0)
+				local target = vim.fn.fnamemodify(current_file, ":p:r") .. ".pdf"
+				local command = { "pandoc", current_file, "-o", target, "--pdf-engine=typst" }
+				local on_success = function(output)
+					print("Success!")
+					vim.api.nvim_echo({ { table.concat(output, "\n") } }, true, {})
+				end
+				local on_error = function(err)
+					print("Error!")
+					vim.api.nvim_echo({ { table.concat(err, "\n"), "ErrorMsg" } }, true, {})
+				end
+				return exporter(command, target, on_success, on_error)
+			end,
+		},
+	},
 })
 vim.lsp.enable("org")
 
